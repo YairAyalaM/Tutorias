980ed47403bb6ac67a7057e16fc8e34035c675b6
YairAyalaM <platagugu@gmail.com>
2022-11-14 12:47:14 -0600

    add table users

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -3,11 +3,13 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
 
 class Users extends Component
 {
     public function render()
     {
+        $this->users = User::all();
         return view('livewire.users');
     }
 }
