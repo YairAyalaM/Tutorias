e47e344eba147dfb085d74dbf00df5f67453187c
YairAyalaM <platagugu@gmail.com>
2022-11-19 13:47:18 -0600

    add sorttable

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -7,23 +7,23 @@
 	<div class="flex items-center justify-center min-h-screen bg-gray-900">
 		<div class="col-span-12">
 			<div class="overflow-auto lg:overflow-visible ">
-			<div class="flex justify-between items-center pb-4 bg-white dark:bg-gray-900">
-			@if($modal)
-            @include('livewire.crear')
-            @endif
-                    <div>
-                        <button wire:click="crear()" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Nuevo</button>
-                    </div>
-                    <label for="table-search" class="sr-only">Search</label>
-                    <div class="relative">
-                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
-                            </svg>
-                        </div>
-                        <input type="text" id="table-search-users" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
-                    </div>
-                </div>
+				<div class="flex justify-between items-center pb-4 bg-white dark:bg-gray-900">
+					@if($modal)
+					@include('livewire.crear')
+					@endif
+					<div>
+						<button wire:click="crear()" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Nuevo</button>
+					</div>
+					<label for="table-search" class="sr-only">Search</label>
+					<div class="relative">
+						<div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+							<svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+							</svg>
+						</div>
+						<input type="text" id="table-search-users" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
+					</div>
+				</div>
 				<table class="table text-gray-400 border-separate space-y-6 text-sm">
 					<thead class="bg-gray-800 text-gray-500">
 						<tr>
@@ -34,12 +34,12 @@
 							<th class="p-3 text-left">Action</th>
 						</tr>
 					</thead>
-					<tbody>
+					<tbody id="table">
 						@foreach($users as $user)
 						<tr class="bg-gray-800">
 							<td class="p-3">
 								<div class="flex align-items-center">
-									<img class="rounded-full h-12 w-12  object-cover" src="https://images.unsplash.com/photo-1613588718956-c2e80305bf61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="unsplash image">
+									<img class="handle cursor-grab rounded-full h-12 w-12  object-cover" src="https://images.unsplash.com/photo-1613588718956-c2e80305bf61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="unsplash image">
 									<div class="ml-3">
 										<div class="">{{$user->name}}</div>
 										<div class="text-gray-500">{{$user->email}}</div>
@@ -99,6 +99,16 @@
 	</style>
 	<!-- fin de componente tabla -->
 	@livewireScripts
+	<!-- script sortable -->
+	<script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+
+	<script>
+		new Sortable(table, {
+			handle: '.handle', //handle permite mover solo elementos que tengan la clase handle
+			animation: 150,
+			ghostClass: 'bg-blue-100'
+		});
+	</script>
 	<!-- scripts de se a eliminado correctamente -->
 	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<x-livewire-alert::scripts />
